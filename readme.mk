f41cbcc14892300230775b4a9b5f68e28e831cf0
Renato Ganske <renatoganske.jr@gmail.com>
2021-12-03 00:10:22 -0300

    first commit

---
+++ readme.mk
@@ -0,0 +1,4 @@
+#Formulario para cadastro de Devs
+
+Formulário criado a partir da aula da Rafaella Ballerini visando praticar e aprofundar os estudos em HTML e CSS.
+Procurei ir além da aula aplicando mudanças no design do formulário, principalmente inserindo um fundo apenas no cabeçalho, o qual tem a largura do formulário.
